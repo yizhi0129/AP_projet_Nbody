2dfbd3586670b3eb279256b7f9336fb19e33a40a
mrvny <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>
2024-01-19 05:15:04 +0100

    add files *.info

--- src/cpu.sh
+++ src/cpu.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cpupower -c all frequency-set -g performance
+sudo cpupower -c all frequency-set -g performance
+
 cpupower -c all frequency-info > ../info/frequency.info
 
 lscpu > ../info/cpu.info
 
-cat /sys/devices/system/cpu/cpu0/cache/index0/* > L1.info
-cat /sys/devices/system/cpu/cpu0/cache/index2/* > L2.info
-cat /sys/devices/system/cpu/cpu0/cache/index3/* > L3.info
+cat /sys/devices/system/cpu/cpu0/cache/index0/* > ../info/L1.info
+cat /sys/devices/system/cpu/cpu0/cache/index2/* > ../info/L2.info
+cat /sys/devices/system/cpu/cpu0/cache/index3/* > ../info/L3.info
 
     
