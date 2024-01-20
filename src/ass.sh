8a0aae2b3276c7cbc3caba9a71eb007388e36311
mrvny <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>
2024-01-21 00:48:20 +0100

    execution on ubuntu

--- src/ass.sh
+++ src/ass.sh
@@ -4,6 +4,8 @@ gcc -S nbody.c
 gcc -S base_gcc.s nbody_base.c
 gcc -S opt_gcc.s nbody_opt.c
 
+mv *.s ../assembly/
+
 clang -S base_clang.s nbody_base.c
 clang -S opt_clang.s nbody_opt.c
 
--- src/clang.sh
+++ src/clang.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-clang -march=native -g3 -O0 -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm
+clang -march=native -g3 -O0 -fopenmp nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_clang_O0.txt
 rm -Rf *~ nbody_base
 
-clang -march=native -g3 -O2 -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm
+clang -march=native -g3 -O2 -fopenmp nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_clang_O2.txt
 rm -Rf *~ nbody_base
 
-clang -march=native -g3 -O3 -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm
+clang -march=native -g3 -O3 -fopenmp nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_clang_O3.txt
 rm -Rf *~ nbody_base
 
-clang -march=native -g3 -Ofast -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm
+clang -march=native -g3 -Ofast -fopenmp nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_clang_Ofast.txt
 rm -Rf *~ nbody_base
 
-clang -march=native -g3 -O0 -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm
+clang -march=native -g3 -O0 -fopenmp nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_O0.txt
 rm -Rf *~ nbody_opt
 
-clang -march=native -g3 -O2 -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm
+clang -march=native -g3 -O2 -fopenmp nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_O2.txt
 rm -Rf *~ nbody_opt
 
-clang -march=native -g3 -O3 -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm
+clang -march=native -g3 -O3 -fopenmp nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_O3.txt
 rm -Rf *~ nbody_opt
 
-clang -march=native -g3 -Ofast -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm
+clang -march=native -g3 -Ofast -fopenmp nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_Ofast.txt
 rm -Rf *~ nbody_opt
 
