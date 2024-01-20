644e20324f2eb1523cd621f9eea3a45362f23787
mrvny <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>
2024-01-20 23:32:34 +0100

    add -S to generate assembly code

--- src/gcc.sh
+++ src/gcc.sh
@@ -3,30 +3,30 @@ make
 taskset -c 3 ./nbody3D 1000 > ../ref/base_gcc_O0.txt
 make clean
 
-gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm
+gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm -S
 taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_O2.txt
 rm -Rf *~ nbody_base *.optrpt
 
-gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm
+gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm -S
 taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_O3.txt
 rm -Rf *~ nbody_base *.optrpt
 
-gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm
+gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm -S
 taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_Ofast.txt
 rm -Rf *~ nbody_base *.optrpt
 
-gcc -march=native -g3 -O0 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
+gcc -march=native -g3 -O0 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm -S
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_O0.txt
 rm -Rf *~ nbody_opt *.optrpt
 
-gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
+gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm -S
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_O2.txt
 rm -Rf *~ nbody_opt *.optrpt
 
-gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
+gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm -S
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_O3.txt
 rm -Rf *~ nbody_opt *.optrpt
 
-gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
+gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm -S
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_Ofast.txt
 rm -Rf *~ nbody_opt *.optrpt
