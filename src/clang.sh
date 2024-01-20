ffa85f4133affa73059a11b758b1a1b6fe619ceb
mrvny <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>
2024-01-21 00:01:14 +0100

    assembly code

--- src/clang.sh
+++ src/clang.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-clang -march=native -g3 -O0 -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm -S
+clang -march=native -g3 -O0 -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_clang_O0.txt
 rm -Rf *~ nbody_base
 
-clang -march=native -g3 -O2 -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm -S
+clang -march=native -g3 -O2 -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_clang_O2.txt
 rm -Rf *~ nbody_base
 
-clang -march=native -g3 -O3 -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm -S
+clang -march=native -g3 -O3 -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_clang_O3.txt
 rm -Rf *~ nbody_base
 
-clang -march=native -g3 -Ofast -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm -S
+clang -march=native -g3 -Ofast -fopenmp -Rpass-analysis=loop-vectorize nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_clang_Ofast.txt
 rm -Rf *~ nbody_base
 
-clang -march=native -g3 -O0 -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm -S
+clang -march=native -g3 -O0 -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_O0.txt
 rm -Rf *~ nbody_opt
 
-clang -march=native -g3 -O2 -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm -S
+clang -march=native -g3 -O2 -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_O2.txt
 rm -Rf *~ nbody_opt
 
-clang -march=native -g3 -O3 -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm -S
+clang -march=native -g3 -O3 -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_O3.txt
 rm -Rf *~ nbody_opt
 
-clang -march=native -g3 -Ofast -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm -S
+clang -march=native -g3 -Ofast -fopenmp -Rpass-analysis=loop-vectorize nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_Ofast.txt
 rm -Rf *~ nbody_opt
+
+clang -S nbody_base.c
+clang -S nbody_opt.c
--- src/gcc.sh
+++ src/gcc.sh
@@ -3,30 +3,34 @@ make
 taskset -c 3 ./nbody3D 1000 > ../ref/base_gcc_O0.txt
 make clean
 
-gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm -S
+gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_O2.txt
 rm -Rf *~ nbody_base *.optrpt
 
-gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm -S
+gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_O3.txt
 rm -Rf *~ nbody_base *.optrpt
 
-gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm -S
+gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm
 taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_Ofast.txt
 rm -Rf *~ nbody_base *.optrpt
 
-gcc -march=native -g3 -O0 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm -S
+gcc -march=native -g3 -O0 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_O0.txt
 rm -Rf *~ nbody_opt *.optrpt
 
-gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm -S
+gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_O2.txt
 rm -Rf *~ nbody_opt *.optrpt
 
-gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm -S
+gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_O3.txt
 rm -Rf *~ nbody_opt *.optrpt
 
-gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm -S
+gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
 taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_Ofast.txt
 rm -Rf *~ nbody_opt *.optrpt
+
+gcc -S nbody.c
+gcc -S nbody_base.c
+gcc -S nbody_opt.c
