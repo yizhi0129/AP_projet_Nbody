72b1ea2363a5437829d7d3a2ca2534835911339c
mrvny <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>
2024-01-21 21:35:27 +0100

    numa < normal, why?

--- src/clang.sh
+++ src/clang.sh
@@ -18,19 +18,19 @@ rm -Rf *~ nbody_base
 
 
 clang -march=native -g3 -O0 -fopenmp nbody_opt.c -o nbody_opt -lm
-./nbody_opt > ../results/opt_clang_O0.txt
+taskset -c 3 ./nbody_opt > ../results/opt_clang_O0.txt
 rm -Rf *~ nbody_opt
 
 clang -march=native -g3 -O2 -fopenmp nbody_opt.c -o nbody_opt -lm
-./nbody_opt > ../results/opt_clang_O2.txt
+taskset -c 3 ./nbody_opt > ../results/opt_clang_O2.txt
 rm -Rf *~ nbody_opt
 
 clang -march=native -g3 -O3 -fopenmp nbody_opt.c -o nbody_opt -lm
-./nbody_opt > ../results/opt_clang_O3.txt
+taskset -c 3 ./nbody_opt > ../results/opt_clang_O3.txt
 rm -Rf *~ nbody_opt
 
 clang -march=native -g3 -Ofast -fopenmp nbody_opt.c -o nbody_opt -lm
-./nbody_opt > ../results/opt_clang_Ofast.txt
+taskset -c 3 ./nbody_opt > ../results/opt_clang_Ofast.txt
 rm -Rf *~ nbody_opt
 
 
--- src/gcc.sh
+++ src/gcc.sh
@@ -18,19 +18,19 @@ rm -Rf *~ nbody_base *.optrpt
 
 
 gcc -march=native -g3 -O0 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-./nbody_opt > ../results/opt_gcc_O0.txt
+taskset -c 3 ./nbody_opt > ../results/opt_gcc_O0.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-./nbody_opt > ../results/opt_gcc_O2.txt
+taskset -c 3 ./nbody_opt > ../results/opt_gcc_O2.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-./nbody_opt > ../results/opt_gcc_O3.txt
+taskset -c 3 ./nbody_opt > ../results/opt_gcc_O3.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-./nbody_opt > ../results/opt_gcc_Ofast.txt
+taskset -c 3 ./nbody_opt > ../results/opt_gcc_Ofast.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 
