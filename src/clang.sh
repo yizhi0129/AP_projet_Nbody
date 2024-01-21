9c307549afdd28dbfc7a24cbe7713b63350b0a7a
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-01-21 20:00:02 +0100

    numa

--- src/clang.sh
+++ src/clang.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 clang -march=native -g3 -O0 -fopenmp nbody_base.c -o nbody_base -lm
-taskset -c 3 ./nbody_base 1000 > ../results/base_clang_O0.txt
+taskset -c 3 ./nbody_base > ../results/base_clang_O0.txt
 rm -Rf *~ nbody_base
 
 clang -march=native -g3 -O2 -fopenmp nbody_base.c -o nbody_base -lm
-taskset -c 3 ./nbody_base 1000 > ../results/base_clang_O2.txt
+taskset -c 3 ./nbody_base > ../results/base_clang_O2.txt
 rm -Rf *~ nbody_base
 
 clang -march=native -g3 -O3 -fopenmp nbody_base.c -o nbody_base -lm
-taskset -c 3 ./nbody_base 1000 > ../results/base_clang_O3.txt
+taskset -c 3 ./nbody_base > ../results/base_clang_O3.txt
 rm -Rf *~ nbody_base
 
 clang -march=native -g3 -Ofast -fopenmp nbody_base.c -o nbody_base -lm
-taskset -c 3 ./nbody_base 1000 > ../results/base_clang_Ofast.txt
+taskset -c 3 ./nbody_base > ../results/base_clang_Ofast.txt
 rm -Rf *~ nbody_base
 
 clang -march=native -g3 -O0 -fopenmp nbody_opt.c -o nbody_opt -lm
-./nbody_opt 1000 > ../results/opt_clang_O0.txt
+./nbody_opt > ../results/opt_clang_O0.txt
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_clang_O0_numa.txt
 rm -Rf *~ nbody_opt
 
 clang -march=native -g3 -O2 -fopenmp nbody_opt.c -o nbody_opt -lm
-./nbody_opt 1000 > ../results/opt_clang_O2.txt
+./nbody_opt > ../results/opt_clang_O2.txt
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_clang_O2_numa.txt
 rm -Rf *~ nbody_opt
 
 clang -march=native -g3 -O3 -fopenmp nbody_opt.c -o nbody_opt -lm
-./nbody_opt 1000 > ../results/opt_clang_O3.txt
+./nbody_opt > ../results/opt_clang_O3.txt
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_clang_O3_numa.txt
 rm -Rf *~ nbody_opt
 
 clang -march=native -g3 -Ofast -fopenmp nbody_opt.c -o nbody_opt -lm
-./nbody_opt 1000 > ../results/opt_clang_Ofast.txt
+./nbody_opt > ../results/opt_clang_Ofast.txt
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_clang_Ofast_numa.txt
 rm -Rf *~ nbody_opt
-
 
--- src/gcc.sh
+++ src/gcc.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 make
-taskset -c 3 ./nbody3D 1000 > ../ref/base_gcc_O0.txt
+taskset -c 3 ./nbody3D > ../ref/base_gcc_O0.txt
 make clean
 
 gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm
-taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_O2.txt
+taskset -c 3 ./nbody_base > ../results/base_gcc_O2.txt
 rm -Rf *~ nbody_base *.optrpt
 
 gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm
-taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_O3.txt
+taskset -c 3 ./nbody_base > ../results/base_gcc_O3.txt
 rm -Rf *~ nbody_base *.optrpt
 
 gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base.c -o nbody_base -lm
-taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_Ofast.txt
+taskset -c 3 ./nbody_base > ../results/base_gcc_Ofast.txt
 rm -Rf *~ nbody_base *.optrpt
 
 gcc -march=native -g3 -O0 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-./nbody_opt 1000 > ../results/opt_gcc_O0.txt
+./nbody_opt > ../results/opt_gcc_O0.txt
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_O0_numa.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-./nbody_opt 1000 > ../results/opt_gcc_O2.txt
+./nbody_opt > ../results/opt_gcc_O2.txt
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_O2_numa.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-./nbody_opt 1000 > ../results/opt_gcc_O3.txt
+./nbody_opt > ../results/opt_gcc_O3.txt
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_O3_numa.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-./nbody_opt 1000 > ../results/opt_gcc_Ofast.txt
+./nbody_opt > ../results/opt_gcc_Ofast.txt
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_Ofast_numa.txt
 rm -Rf *~ nbody_opt *.optrpt
-
