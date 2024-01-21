1e967c64a5d8cab44a56f7545d7ce4d33fd459a2
mrvny <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>
2024-01-21 01:04:57 +0100

    rm 'taskset -c 3' for opt version

--- src/clang.sh
+++ src/clang.sh
@@ -16,19 +16,19 @@ taskset -c 3 ./nbody_base 1000 > ../results/base_clang_Ofast.txt
 rm -Rf *~ nbody_base
 
 clang -march=native -g3 -O0 -fopenmp nbody_opt.c -o nbody_opt -lm
-taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_O0.txt
+./nbody_opt 1000 > ../results/opt_clang_O0.txt
 rm -Rf *~ nbody_opt
 
 clang -march=native -g3 -O2 -fopenmp nbody_opt.c -o nbody_opt -lm
-taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_O2.txt
+./nbody_opt 1000 > ../results/opt_clang_O2.txt
 rm -Rf *~ nbody_opt
 
 clang -march=native -g3 -O3 -fopenmp nbody_opt.c -o nbody_opt -lm
-taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_O3.txt
+./nbody_opt 1000 > ../results/opt_clang_O3.txt
 rm -Rf *~ nbody_opt
 
 clang -march=native -g3 -Ofast -fopenmp nbody_opt.c -o nbody_opt -lm
-taskset -c 3 ./nbody_opt 1000 > ../results/opt_clang_Ofast.txt
+./nbody_opt 1000 > ../results/opt_clang_Ofast.txt
 rm -Rf *~ nbody_opt
 
 
--- src/gcc.sh
+++ src/gcc.sh
@@ -16,18 +16,18 @@ taskset -c 3 ./nbody_base 1000 > ../results/base_gcc_Ofast.txt
 rm -Rf *~ nbody_base *.optrpt
 
 gcc -march=native -g3 -O0 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_O0.txt
+./nbody_opt 1000 > ../results/opt_gcc_O0.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_O2.txt
+./nbody_opt 1000 > ../results/opt_gcc_O2.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_O3.txt
+./nbody_opt 1000 > ../results/opt_gcc_O3.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
-taskset -c 3 ./nbody_opt 1000 > ../results/opt_gcc_Ofast.txt
+./nbody_opt 1000 > ../results/opt_gcc_Ofast.txt
 rm -Rf *~ nbody_opt *.optrpt
 
