59504e5ef7354ed7033cf67292a4beb0a83c5d07
mrvny <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>
2024-01-21 20:43:32 +0100

    modifiy shell script

--- src/gcc.sh
+++ src/gcc.sh
@@ -15,22 +15,38 @@ gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_base
 taskset -c 3 ./nbody_base > ../results/base_gcc_Ofast.txt
 rm -Rf *~ nbody_base *.optrpt
 
+
+
 gcc -march=native -g3 -O0 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
 ./nbody_opt > ../results/opt_gcc_O0.txt
-OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_O0_numa.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
 ./nbody_opt > ../results/opt_gcc_O2.txt
-OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_O2_numa.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
 ./nbody_opt > ../results/opt_gcc_O3.txt
-OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_O3_numa.txt
 rm -Rf *~ nbody_opt *.optrpt
 
 gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
 ./nbody_opt > ../results/opt_gcc_Ofast.txt
+rm -Rf *~ nbody_opt *.optrpt
+
+
+
+gcc -march=native -g3 -O0 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_O0_numa.txt
+rm -Rf *~ nbody_opt *.optrpt
+
+gcc -march=native -g3 -O2 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_O2_numa.txt
+rm -Rf *~ nbody_opt *.optrpt
+
+gcc -march=native -g3 -O3 -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
+OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_O3_numa.txt
+rm -Rf *~ nbody_opt *.optrpt
+
+gcc -march=native -g3 -Ofast -fopenmp -fopt-info-all=nbody.gcc.optrpt nbody_opt.c -o nbody_opt -lm
 OMP_PROC_BIND=close OMP_PLACES=cores numactl --membind=0 ./nbody_opt > ../results/opt_gcc_Ofast_numa.txt
 rm -Rf *~ nbody_opt *.optrpt
